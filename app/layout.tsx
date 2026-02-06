ee5d8cdf7f5fe79bb3266cac92f2a934c30e3887
Juan Piris <jctec050@gmail.com>
2026-02-06 20:53:39 -0300

    se corrigió el nombre de la aplicacion y se agregó logo para favicon para busqueda en google

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,13 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-    title: 'TuCancha! - Reserva tu Cancha',
+    title: 'TuCancha! - Reservá tu Cancha',
     description: 'Sistema de gestión y reserva de canchas de padel y beach tennis',
+    icons: {
+        icon: '/logo.png',
+        shortcut: '/logo.png',
+        apple: '/logo.png',
+    },
 };
 
 export default function RootLayout({
